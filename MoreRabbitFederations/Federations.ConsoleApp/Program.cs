e24b8a2e80635a2a5742d79a7e0b09c7ce399697
Christopher Loggins <christopher.loggins@gmail.com>
2014-01-21 14:35:10 -0600

    Another Federation Example
    Starting another example of federating two brokers.

---
+++ MoreRabbitFederations/Federations.ConsoleApp/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Federations.ConsoleApp
+{
+    using System;
+
+    using Properties;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            try
+            {
+
+            }
+            catch (Exception exception)
+            {
+                DisplayException(exception);
+            }
+            finally
+            {
+                DisplayExitPrompt();
+            }
+        }
+
+        private static void DisplayException(Exception exception)
+        {
+            Console.WriteLine();
+            Console.WriteLine(exception);
+        }
+
+        private static void DisplayExitPrompt()
+        {
+            Console.WriteLine();
+            Console.WriteLine(Resources.ExitMessage);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MoreRabbitFederations/Federations.ConsoleApp/WindsorFactory.cs
@@ -0,0 +1,19 @@
+﻿namespace Federations.ConsoleApp
+{
+    using Castle.Windsor;
+    using Castle.Windsor.Installer;
+
+    public static class WindsorFactory
+    {
+        public static IWindsorContainer Create()
+        {
+            var container = new WindsorContainer()
+                .Install(
+                    Configuration.FromAppConfig(),
+                    FromAssembly.This()
+                );
+
+            return container;
+        }
+    }
+}
