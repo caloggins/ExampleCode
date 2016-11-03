59938a36f989a894e98d243c54ee7f4d9f0c100a
Christopher Loggins <cloggins@paylocity.com>
2016-11-03 08:36:06 -0500

    Added basic app.

---
+++ NancyWithTokens/NancyWithTokens.WebSite.Tests/HealthTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Nancy;
+using Nancy.Testing;
+using NUnit.Framework;
+
+namespace NancyWithTokens.WebSite.Tests
+{
+    public class HealthTests
+    {
+        [Test]
+        public void ItShouldReturnTheCorrectStatusCode()
+        {
+            var browser = new Browser(with => { with.Module<Health>(); });
+
+            var response = browser.Get("/", with => { with.HttpsRequest(); });
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+    }
+}
---
+++ NancyWithTokens/NancyWithTokens.WebSite/Health.cs
@@ -0,0 +1,12 @@
+﻿using Nancy;
+
+namespace NancyWithTokens.WebSite
+{
+    public class Health : NancyModule
+    {
+        public Health()
+        {
+            Get["/"] = o => HttpStatusCode.OK;
+        }
+    }
+}
